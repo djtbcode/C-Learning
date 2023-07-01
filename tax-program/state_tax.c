b66721f8e73a90e7d8dd4b47f3f7b3439ac2cc51
djtbcode <129532735+djtbcode@users.noreply.github.com>
2023-07-01 09:27:25 +0000

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   tax-program/state_tax 	modified:   tax-program/state_tax.c

--- tax-program/state_tax.c
+++ tax-program/state_tax.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
 int get_sales(void);
+void calc_tax(int sales);
 
 int main(void)
 {
     int sales = get_sales();
-    printf("Total Yearly sales equals: %i", sales);
+    printf("Total Yearly sales equals: %i\n", sales);
+    calc_tax(sales);
+    return 0;
 }
 
 int get_sales(void)
 {
     int sales = 0;
-    sales = get_int("Please enter total yearly sales: ");
-    return 0;
+    printf("Please enter total yearly sales: ");
+    scanf("%d", &sales);
+    return sales;
+}
+
+void calc_tax(int sales)
+{
+    const double state_tax_rate = .04;
+    const double local_tax_rate = .02;
+
+    double state_tax = state_tax_rate * sales;
+    printf("State Tax Amount Due: %.2f\n", state_tax);
+    double local_tax = local_tax_rate * sales; 
+    printf("Local Tax Amount due: %.2f\n", local_tax);
+    double total_tax = local_tax + state_tax;
+    printf("Total Tax Due: %.2f\n", total_tax);
+    double after_tax = sales - total_tax;
+    printf("After Tax Income: %.2f\n", after_tax);
 }
